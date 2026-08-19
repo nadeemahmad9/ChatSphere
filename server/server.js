c04dcde5a6d47648b2f5a78ed8d6074ad08587ef
nadeem <nadeemahmad199726@gmail.com>
2026-08-19 10:26:24 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -182,7 +182,7 @@ app.use(express.json());
 // =========================
 // Port
 // =========================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // =========================
 // Basic Route
