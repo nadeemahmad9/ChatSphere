7713a3efc83b122ac4710c3f2426ddc44b05339f
nadeem <nadeemahmad199726@gmail.com>
2026-08-20 19:24:18 +0530

    userServices

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,22 +1,23 @@
+const API_URL = `${import.meta.env.VITE_API_URL}/api/auth`;
+
+
 export const fetchUsers = async () => {
     try {
-        // API call
         const response = await fetch(
-            "/api/auth/users",
-        {
-            method: "GET",
-            credentials: "include",
-        }
+            `${API_URL}/users`,
+            {
+                method: "GET",
+                credentials: "include",
+            }
         );
 
         const data = await response.json();
+
         return data;
 
     } catch (error) {
-
         console.error("Fetch Users Error:", error);
 
         throw error;
-
     }
 };
