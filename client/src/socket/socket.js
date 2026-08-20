8f8362466facc98e25333e040a3ec7250183a130
nadeem <nadeemahmad199726@gmail.com>
2026-08-20 15:31:59 +0530

    Fix production Socket.IO connection

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
 
-console.log("Socket instance created");
+// console.log("Socket instance created");
 
 export const socket = io(import.meta.env.VITE_API_URL, {
     autoConnect: false,
     withCredentials: true,
+    transports: ["websocket"],
 });
