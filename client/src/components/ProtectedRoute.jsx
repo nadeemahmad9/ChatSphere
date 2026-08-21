38c7c2d0497ebe59fcfb15a83249848904ca3130
nadeem <nadeemahmad199726@gmail.com>
2026-08-21 10:10:24 +0530

    debugging

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -6,17 +6,20 @@ export const ProtectedRoute = ({ children }) => {
 
     const { loading, isAuthenticated } = useContext(AuthContext);
 
-    console.log("PROTECTED ROUTE:", {
+    console.log("🔥 PROTECTED ROUTE RENDERED", {
         loading,
         isAuthenticated,
         path: window.location.pathname,
-    });
+        children,
+    })
 
     if (loading) {
         return <h1>Loading...</h1>;
     }
 
     if (!isAuthenticated) {
+        console.log("🚨 PROTECTED ROUTE REDIRECTING TO LOGIN");
+
         return <Navigate to="/login" replace />;
     }
 
