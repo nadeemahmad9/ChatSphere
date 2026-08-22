e15318c8defd2786c675c13c98aa5acc49b0ece9
nadeem <nadeemahmad199726@gmail.com>
2026-08-22 12:15:53 +0530

    outside close useeffect

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useChat } from "../../context/ChatContext";
 import {
     ArrowLeft,
